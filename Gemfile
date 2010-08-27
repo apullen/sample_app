6adb048f914df02cd5f54a46341864489fbca7ee
Angelo Pullen <apullen@mac.com>
2010-08-27 02:53:22 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
+gem 'gravatar_image_tag', '0.1.0'
+gem 'will_paginate', '>= 3.0.pre'
+
+group :development do
+  gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'annotate-models'
+  gem 'faker', '0.3.1'
+end
+
+group :test do
+  gem 'rspec',       '2.0.0.beta.18'
+  gem 'spork', '0.8.4'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0e82c6a9b8baf57b9e1174012a333e5e06d3159afca1e3dbf20ed4f412d7b6e8eb6d8d536f0dbc93eb180857d1eb52472c0332e353c246c03aaf639629c6774f'
